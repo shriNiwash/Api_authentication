f83ae3ecea51c56d8e2a9709830e6773075ee4fd
shriNiwash <shriniwashsharma98413@gmail.com>
2022-05-06 16:07:52 +0545

    commited

--- model/db.js
+++ model/db.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 //Now Making the connection with database
-const conn =process.env.connection;
+// const conn ="mongodb+srv://shriNiwash:Mriphone12345@cluster0.waox8.mongodb.net/MERN_SHRI?retryWrites=true&w=majority";
+const conn = process.env.CONNECTION;
+console.log(conn);
 mongoose.connect(conn,{useNewUrlParser: true,useUnifiedTopology:true}).
 then(()=>console.log("The database Conenction is successfull")).catch((err)=>console.log(err));
 
